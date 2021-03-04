281c4a4ef5fdeb73e16cd48568a33d212de866ef
Jesusa SADEY <justinjhes@gmail.com>
2021-03-04 16:19:07 +0100

    les images des fruits, et creation de layout tentative utilisateur

---
+++ app/src/main/java/com/example/mastermind/FruitAdapter.java
@@ -0,0 +1,15 @@
+package com.example.mastermind;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FruitAdapter extends  RecyclerView.Adapter<FruitAdapter.FruitHolder>{
+
+    public class
+
+}
