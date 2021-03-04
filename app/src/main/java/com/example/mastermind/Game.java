bb64e5c50add4bc38dcc3d5cf817473bdef98ceb
TanKARASU <karasutan@gmail.com>
2021-03-04 10:23:41 +0100

    function user_attempt_checker && function generate_answer ok

---
+++ app/src/main/java/com/example/mastermind/Game.java
@@ -0,0 +1,92 @@
+package com.example.mastermind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+public class Game extends AppCompatActivity {
+    // list of available fruit
+    Fruit Banana = new Fruit("Banana", false, true, "url");
+    Fruit Kiwi = new Fruit("Kiwi", false, true, "url");
+    Fruit Strawberry = new Fruit("Strawberry", false, false, "url");
+    Fruit Raspberry = new Fruit("Raspberry", false, false, "url");
+    Fruit Grapes = new Fruit("Grapes", true, false, "url");
+    Fruit Orange = new Fruit("Orange", false, true, "url");
+    Fruit Lemon = new Fruit("Lemon", false, true, "url");
+    Fruit Plum = new Fruit("Plum", true, false, "url");
+
+    // Fruit basket = Fruits
+    Fruit[] Fruits = {Banana, Kiwi, Strawberry, Raspberry, Grapes, Orange, Lemon, Plum};
+    Fruit[] generated_answer = generate_answer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        String[] test = {"Lemon", "Strawberry", "Orange", "Raspberry"};
+        user_attempt_checker(generate_answer(), test);
+    }
+
+    public Fruit[] generate_answer() {
+        int inserted_fruit = 0;
+        Fruit[] result = new Fruit[4];
+
+        while (inserted_fruit != 4) {
+            Random rnd = new Random();
+            int proposition = rnd.nextInt(Fruits.length);
+            boolean isValid = true;
+
+            for (int i = 0; i < result.length; i++) {
+                if (result[i] != null) {
+                    if (result[i].getName().equals(Fruits[proposition].getName())) {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isValid) {
+                result[inserted_fruit] = Fruits[proposition];
+                inserted_fruit++;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Function who take a generated answer in argument and check if the attempt is ok
+     * p_generated_answer : returned by generate_answer()
+     * p_user_input : an array of string who represent the user answer
+     * return :
+     *
+     * @return an array of integer
+     */
+    public Integer[] user_attempt_checker(Fruit[] p_generated_answer, String[] p_user_input) {
+        Integer[] result = new Integer[4];
+        // on boucle sur les resultats de l'utilisateur
+        // si le fruit est absent on met 0 dans un tableau
+        for (int i = 0; i < p_user_input.length; i++) {
+            int checked_result = 0;
+            for (int j = 0; j < p_generated_answer.length; j++) {
+                if (p_user_input[i].equals(p_generated_answer[j].getName())) {
+                    if (i == j) {
+                        // sil le fruit est présent et bien placé on met 2 dans un tableau
+                        checked_result = 2;
+                    } else {
+                        // si le fruit est présent on met 1 dans un tableau
+                        checked_result = 1;
+                    }
+                }
+            }
+            result[i] = checked_result;
+        }
+        // on fait un sort() descendant sur le tableau
+        Arrays.sort(result, Collections.reverseOrder());
+        // on retourne le tableau ex: [2,1,0,0]
+        return result;
+    }
+}
